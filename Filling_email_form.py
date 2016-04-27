2d81c5a759b90d7cc11e5074f6c7727ece1916a3
Vadim Potapov <doctorvra4@gmail.com>
2016-04-27 15:22:09 +0600

    edit Filling_email_form class

--- Filling_email_form.py
+++ Filling_email_form.py
@@ -2,6 +2,7 @@ from Head import Head
 class Filling_email_form(Head):
 
     def enter_email(self, text):
+        self.email = self.driver.find_element_by_name("Email")
         self.email.send_keys(text)
         self.email.submit()
 
