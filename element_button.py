18d652db085dfefff3335d63fe553d60fafd82c5
ast-ivanov <arch.step.inc@gmail.com>
2016-04-27 14:09:01 +0600

    Delete element_button.py

--- element_button.py
+++
@@ -1,6 +0,0 @@
-from base_component import BaseComponent
-
-
-class Button(BaseComponent):
-    def click(self, element_class_name):
-        self.driver.find_element_by_class_name(element_class_name).click()
