47c0dd3f70a914c4ca51a71e5c6d86ae2cc4da99
Andrey Kargapolov <bilininandroid@gmail.com>
2016-04-28 09:38:42 +0700

    Изменил файл в соответствии с подходом Component&PageObject

--- main_l8.py
+++ main_l8.py
@@ -1,4 +1,6 @@
 import unittest
+from login_page import LoginPage
+from main_page import MainPage
 from selenium import webdriver
 from unittest import TestCase
 
@@ -14,11 +16,9 @@ class Test(TestCase):
         passwd = "2GisKargapolovTestTest"
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(5)
-        self.driver.get("http://mail.google.com/")
-        self.driver.find_element_by_id("Email").send_keys(email)
-        self.driver.find_element_by_id("next").click()
-        self.driver.find_element_by_id("Passwd").send_keys(passwd)
-        self.driver.find_element_by_id("signIn").click()
+        login_page = LoginPage(self.driver)
+        login_page.open("http://mail.google.com/")
+        login_page.login_form.login(email, passwd)
 
     def test_no_exist(self):
         """
@@ -29,9 +29,9 @@ class Test(TestCase):
             Вывод сообщения о том, что писем с таким текстом в ящике не найдено.
         """
         search_text = "some_text"
-        self.driver.find_element_by_id("gbqfq").send_keys(search_text)
-        self.driver.find_element_by_id('gbqfb').click()
-        self.assertIn("Писем не найдено.", self.driver.page_source)
+        page = MainPage(self.driver)
+        page.search_bar.search(search_text)
+        self.assertIn("Писем не найдено.", page.page_source)
 
     def test_overflow(self):
         """
@@ -40,9 +40,9 @@ class Test(TestCase):
             2)Нажать Enter.
         """
         search_text = "overflow" * 400
-        self.driver.find_element_by_id("gbqfq").send_keys(search_text)
-        self.driver.find_element_by_id('gbqfb').click()
-        self.assertIn("Слишком длинный поисковый запрос. Сократите его.", self.driver.page_source)
+        page = MainPage(self.driver)
+        page.search_bar.search(search_text)
+        self.assertIn("Слишком длинный поисковый запрос. Сократите его.", page.page_source)
 
     def test_gramm_mistake(self):
         """
@@ -51,9 +51,9 @@ class Test(TestCase):
                 2)Нажать Enter.
             """
         search_text = "молако"
-        self.driver.find_element_by_id("gbqfq").send_keys(search_text)
-        self.driver.find_element_by_id('gbqfb').click()
-        self.assertIn("Возможно, вы имели в виду:", self.driver.page_source)
+        page = MainPage(self.driver)
+        page.search_bar.search(search_text)
+        self.assertIn("Возможно, вы имели в виду:", page.page_source)
 
     def test_size_mistake(self):
         """
@@ -63,11 +63,11 @@ class Test(TestCase):
                 2)Нажать Enter.
         ОжидаемыЙ результат: Сообщение "Неверный запрос поиска – возврат всех писем"
         """
-        search_text = "some_string"
-        self.driver.find_element_by_id('gbqfab').click()
-        self.driver.find_element_by_xpath("//input[@aria-label='Значение размера']").send_keys(search_text)
-        self.driver.find_element_by_id('gbqfb').click()
-        self.assertIn("Неверный запрос поиска", self.driver.page_source)
+        size = "some_text"
+        page = MainPage(self.driver)
+        page.search_bar.add_param_size(size)
+        page.search_bar.search("")
+        self.assertIn("Неверный запрос поиска – возврат всех писем", page.page_source)
 
     def test_from(self):
         """
@@ -77,11 +77,11 @@ class Test(TestCase):
                 2)Нажать Enter.
         ОжидаемыЙ результат: Вывод списка сообщений от Gmail.
         """
-        search_text = "Gmail"
-        self.driver.find_element_by_id('gbqfab').click()
-        self.driver.find_element_by_xpath("/html/body/div/div/div/div/span/input").send_keys(search_text)
-        self.driver.find_element_by_id('gbqfb').click()
-        self.assertIn("Команда Gmail", self.driver.page_source)
+        page = MainPage(self.driver)
+        from_ = "Gmail"
+        page.search_bar.add_param_from(from_)
+        page.search_bar.search("")
+        self.assertIn("Команда Gmail", page.page_source)
 
     def tearDown(self):
         self.driver.quit()
